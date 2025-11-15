14fc3cae672218245447a0ccebdf1bb10916a95a
Arthur Jardot <arthur.jardot@gmail.com>
2025-11-15 12:19:15 +0900

    now both import files allowed to correctly import all my strava data

--- import.py
+++ import.py
@@ -1,14 +1,13 @@
 
-import stravalib
 import os
 from time import time
 from stravalib import Client
 
 #Set your client_id
-MY_STRAVA_CLIENT_ID = int(os.environ.get('CLIENT_ID'))
+MY_STRAVA_CLIENT_ID = int(os.environ.get('STRAVA_CLIENT_ID'))
 
 #Set your client_secret
-MY_STRAVA_CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+MY_STRAVA_CLIENT_SECRET = os.environ.get('STRAVA_CLIENT_SECRET')
 
 client = Client()
 url = client.authorization_url(
@@ -21,20 +20,21 @@ print(url)
 client = Client()
 
 #Use this token to make API calls
-access_token='293a195f57891526a13cc67ada60e6c1048ed9d6'
+access_token='4520415a3e46580ebb4dab2223d712b9accf424b'
 client.access_token = access_token
 
 #Use this token to keep your session alive
-last_refresh_token = 'b693d8a9ef901c1b90955dc1a4b16cdab141c55d'
-client.refresh_token = last_refresh_token
+refresh_token='1cc1daea3458fe337feab64e03971a958d1c1b05'
+client.refresh_token = refresh_token
 
 #Initial expiration time
-client.token_expires_at = 1763034879
+token_expires_at = 1763197179
+client.token_expires = token_expires_at
 
-if time() > client.token_expires_at:
+if time() > client.token_expires:
     refresh_response = client.refresh_access_token(
         client_id=MY_STRAVA_CLIENT_ID,
-        client_secret="TON_CLIENT_SECRET",
+        client_secret=MY_STRAVA_CLIENT_SECRET,
         refresh_token=client.refresh_token
     )
     access_token = refresh_response["access_token"]
@@ -43,8 +43,15 @@ if time() > client.token_expires_at:
 
 client.access_token = access_token
 
+
+
 athlete = client.get_athlete()
-print(f"Athlete: {athlete.firstname} {athlete.lastname}")
+print(f"Bonjour {athlete.firstname} !")
+
+activities = client.get_activities(limit=1)
+for a in activities:
+    print(a.id, a.name)
+
 
 
 
