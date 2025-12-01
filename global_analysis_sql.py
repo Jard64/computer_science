3a03afe0ef88d900a4048e7d25959ef114e06d7c
Arthur Jardot <arthur.jardot@gmail.com>
2025-12-01 18:53:12 +0900

    Add monthly_distance and cadence plots and id to date function

--- global_analysis_sql.py
+++ global_analysis_sql.py
@@ -3,8 +3,9 @@ import numpy as np
 import datetime
 import sqlite3
 from api_call import client
+from functools import partial
 
-conn = sqlite3.connect("sqlite_activity_database_copy.db")
+conn = sqlite3.connect("sqlite_activity_database.db")
 cursor = conn.cursor()
 
 
@@ -19,6 +20,13 @@ def number_of_activities():
     """).fetchone()
     return result[0]
 
+def all_activities_id():
+    """Get all_activities_id from the databse"""
+    result= cursor.execute("""
+        SELECT id FROM activity
+                           ;""").fetchall()
+    list_id=[id[0] for id in result]
+    return list_id
 
 def total_time_hours():
     """Temps total en heures"""
@@ -94,9 +102,27 @@ def get_average_bpm():
     return avg_bpm
 
 
+def get_monthly_distance():
+    """Get total distance per month in km"""
+    activities=cursor.execute("""
+            SELECT start_date, distance FROM activity""")
+    monthly_distance={}
+    for activity in activities:
+        date_str=activity[0].split()[0]
+        date_objet=datetime.datetime.strptime(date_str,"%Y-%m-%d").date()
+        month_years=f"{date_objet.month}-{date_objet.year}"
+        distance=activity[1]/1000
+        if month_years in monthly_distance:
+            monthly_distance[month_years]+=distance
+        else:
+            monthly_distance[month_years]=distance
+    return monthly_distance
+            
+
 def get_dates():
     """Récupère les dates des courses"""
     activities = cursor.execute("""
+        SELECT start_date FROM activity
         WHERE total_elevation_gain IS NOT NULL 
         AND average_heartrate IS NOT NULL
         AND average_speed IS NOT NULL
@@ -112,9 +138,36 @@ def get_dates():
     date = date[::-1]
     return date
 
+def ids_from_dates(list_dates):
+    ids=np.array([])
+    for date in list_dates:
+        result=cursor.execute("""
+            SELECT id FROM activity
+            WHERE start_date LIKE ?;
+        """, (f"{date}%",)).fetchone()
+        if result:
+            ids=np.append(ids,result[0])
+    return ids
+
+def dates_from_ids(list_ids):
+    """Récupère les dates des courses à partir d'une liste d'IDs"""
+    date = np.array([])
+    for activity_id in list_ids:
+        result = cursor.execute("""
+            SELECT start_date FROM activity
+            WHERE id = ?;
+        """, (activity_id,)).fetchone()
+        
+        if result:
+            date_str = result[0].split()[0]
+            date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+            date = np.append(date, date_obj)
+    
+    return date
+
 
 def get_average_pace():
-    """Récupère l'allure moyenne (min/km)"""
+    """Get average pace in (min/km)"""
     activities = cursor.execute("""
         SELECT average_speed FROM activity
         WHERE total_elevation_gain IS NOT NULL 
@@ -135,7 +188,7 @@ def get_average_pace():
 
 
 def get_average_speed():
-    """Récupère la vitesse moyenne (km/h)"""
+    """Get average speed in (km/h)"""
     activities = cursor.execute("""
         SELECT average_speed FROM activity
         WHERE total_elevation_gain IS NOT NULL 
@@ -152,6 +205,24 @@ def get_average_speed():
     avg_speed = avg_speed[::-1]
     return avg_speed
 
+def get_average_cadence():
+    """Get average cadence in (step/min)"""
+    activities = cursor.execute("""
+        SELECT average_cadence FROM activity
+        WHERE total_elevation_gain IS NOT NULL 
+        AND average_heartrate IS NOT NULL
+        AND average_speed IS NOT NULL
+        ORDER BY start_date DESC;
+    """).fetchall()
+    
+    avg_cadence = np.array([])
+    for a in activities:
+        if a[0] is not None:
+            #Convert for cycle cadence to run cadence
+            avg_cadence = np.append(avg_cadence, a[0] * 2) 
+    
+    avg_cadence = avg_cadence[::-1]
+    return avg_cadence
 
 def get_elevation_gain():
     """Récupère le dénivelé positif"""
@@ -170,6 +241,29 @@ def get_elevation_gain():
     elevation_gain = elevation_gain[::-1]
     return elevation_gain
 
+def get_running_effectiveness():
+    """Get running effectiveness (speed/watts/kg)"""
+    activities = cursor.execute("""
+        SELECT average_speed, average_watts,start_date FROM activity
+        WHERE total_elevation_gain IS NOT NULL 
+        AND average_heartrate IS NOT NULL
+        AND average_speed IS NOT NULL
+        AND average_watts IS NOT NULL
+        ORDER BY start_date DESC;
+    """).fetchall()
+    
+    running_effectiveness = np.array([])
+    activity_with_power_date=np.array([])
+    for a in activities:
+        if a[1] is not None and a[1] > 0:
+            weight = 64  # Assume weight has not significantly changed
+            effectiveness = (a[0] * 3.6) / (a[1] / weight)
+            running_effectiveness = np.append(running_effectiveness, effectiveness)
+            activity_with_power_date=np.append(activity_with_power_date,a[2].split()[0])
+    
+    running_effectiveness = running_effectiveness[::-1]
+    activity_with_power_date=activity_with_power_date[::-1]
+    return running_effectiveness,activity_with_power_date
 
 def personal_best_evolution(distance_km=10,min_time=3000):
     activities=client.get_activities()
@@ -197,7 +291,8 @@ def scatter_average_bpm():
     """Nuage de points: FC moyenne vs date"""
     avg_bpm = get_average_bpm()
     date = get_dates()
-    scatter_average_bpm = plt.scatter(date, avg_bpm)
+    scatter_average_bpm = plt.scatter(date, avg_bpm,color="#b30909")
+    plt.xticks(size=8)
     return scatter_average_bpm
 
 
@@ -208,14 +303,33 @@ def plot_bpm_trend():
     reg = np.polyfit(range(len(date)), avg_bpm, 1)
     print(reg)
     plot_bpm = plt.plot(date, np.polyval(reg, range(len(date))), color='red')
+    plt.xticks(size=8)
     return plot_bpm
 
+def scatter_average_cadence():
+    """Scatter plot: Average cadence vs date"""
+    avg_cadence = get_average_cadence()
+    date = get_dates()
+    scatter_average_cadence = plt.scatter(date, avg_cadence,color="#be0744")
+    plt.xticks(size=8)
+    return scatter_average_cadence
+
+def plot_cadence_trend():    
+    """Average cadence trend"""
+    avg_cadence = get_average_cadence()
+    date = get_dates()
+    reg = np.polyfit(range(len(date)), avg_cadence, 1)
+    print(reg)
+    plot_cadence = plt.plot(date, np.polyval(reg, range(len(date))), color='red')
+    plt.xticks(size=8)
+    return plot_cadence
 
 def scatter_average_pace():
     """Nuage de points: Allure vs date"""
     avg_pace = get_average_pace()
     date = get_dates()
     scatter_average_pace = plt.scatter(date, avg_pace)
+    plt.xticks(size=8)
     return scatter_average_pace
 
 
@@ -226,6 +340,7 @@ def plot_pace_trend():
     reg = np.polyfit(range(len(date)), avg_pace, 1)
     print(reg)
     plot_pace = plt.plot(date, np.polyval(reg, range(len(date))), color='red')
+    plt.xticks(size=8)
     return plot_pace
 
 
@@ -234,6 +349,7 @@ def scatter_average_speed():
     avg_speed = get_average_speed()
     date = get_dates()
     scatter_average_speed = plt.scatter(date, avg_speed)
+    plt.xticks(size=8)
     return scatter_average_speed
 
 
@@ -244,6 +360,7 @@ def plot_speed_trend():
     reg = np.polyfit(range(len(date)), avg_speed, 1)
     print(reg)
     plot_speed = plt.plot(date, np.polyval(reg, range(len(date))), color='red')
+    plt.xticks(size=8)
     return plot_speed
 
 
@@ -254,6 +371,7 @@ def scatter_efficiency():
     date = get_dates()
     efficiency = average_bpm / (average_speed) * 60  # New metric: beats per km
     scatter = plt.scatter(date, efficiency)
+    plt.xticks(size=8)
     return scatter
 
 
@@ -266,6 +384,7 @@ def plot_efficiency_trend():
     reg = np.polyfit(range(len(date)), efficiency, 1)
     print(reg)
     plot_efficiency = plt.plot(date, np.polyval(reg, range(len(date))), color='red')
+    plt.xticks(size=8)
     return plot_efficiency
 
 
@@ -306,7 +425,7 @@ def plot_average_bpm_speed_trend(altitude_gain_limit=100):
     plot_bpm_speed = plt.plot(avg_speed, np.polyval(reg, avg_speed), color='red')
     plot_bpm_speed_flat = plt.plot(filtered_avg_speed, np.polyval(reg_flat, filtered_avg_speed), color='green')
     
-    
+    plt.xticks(size=8)
     return plot_bpm_speed
 
 def plot_corrcoef_evolution():
@@ -320,31 +439,143 @@ def plot_corrcoef_evolution():
         filtered_average_bpm=[average_bpm[i] for i in range(len(elevation_gain)) if elevation_gain[i]<alt_gain_limit]
         corr_coefficients.append(np.corrcoef(filtered_avg_speed, filtered_average_bpm)[0,1])
     corrcoef_evolution=plt.plot(np.linspace(10,2000,500),corr_coefficients)
+    plt.xticks(size=8)
     return corrcoef_evolution
 
+def plot_settings(function, title,xlabel,ylabel,filename,grid=False,save=False):
+    
+    plot=function()
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if grid :
+        plt.grid()
+    if save:
+        plt.savefig("./Results/"+filename, dpi=300)
+    
+    return plot
 
+def plot_monthly_distance(save=False):
+    monthly_distance=get_monthly_distance()
+    months=list(monthly_distance.keys())
+    distances=list(monthly_distance.values())
+    plt.plot(months, distances, linestyle='-', color='#FC4C02',
+              marker='o', markeredgecolor='#FC4C02', markerfacecolor='none')
+    plt.xticks(rotation=90)
+    plt.xticks(size=8)
+    plt.xlabel("Month-Year")
+    plt.ylabel("Monthly distance (km)")
+    plt.title("Totale distance run per months")
+    plt.grid(axis='y')
+    save_plt = plt.savefig("./Results/monthly_distance.png",dpi=300) if save else None
+    plt.tight_layout()
+    plt.xticks(size=8)
 
-# ============== EXEMPLE D'UTILISATION ==============
+def scatter_running_effectiveness():
+    """Plot running effectiveness over time"""
+    running_effectiveness,date=get_running_effectiveness()
+    
+    plt.scatter(date,running_effectiveness,color="#d408bc")
+    plt.title("Running effectiveness over time")
+    plt.xlabel("Date")
+    plt.ylabel("Running effectiveness (speed/watts/kg)")
+    plt.tight_layout()
+    plt.xticks(size=8)
+
+def plot_running_effectiveness_trend():
+    """Plot running effectiveness trend"""
+    running_effectiveness,date=get_running_effectiveness()
+    
+    reg = np.polyfit(range(len(date)), running_effectiveness, 1)
+    print(reg)
+    plot_efficiency = plt.plot(date, np.polyval(reg, range(len(date))), color='red')
+    plt.xticks(size=8)
+    plt.tight_layout()
+    return plot_efficiency
+    
+
+# ============== USE CASES ==============
 
 if __name__ == "__main__":
     
     # Exemple de graphique
-    plt.figure()
     
-    plot_corrcoef_evolution()
-    plt.title('Correlation coefficient variation following the data set max elevation')
-    plt.xlabel('elevation gain (m)')
-    plt.ylabel('Correlation coefficient ')
-    plt.grid()
-    plt.savefig('./Results/correlation_coefficient.png', dpi=300)
+    plt.figure()
+    plot_settings(plot_corrcoef_evolution,xlabel='elevation gain (m)',
+                                     ylabel='Correlation coefficient ',
+                                     title='Correlation coefficient variation following the data set max elevation',
+                                     filename='correlation_coefficient_settings.png',
+                                     grid=True,save=False)
+
+
 
     plt.figure()
     scatter_average_bpm_speed()
-    plot_average_bpm_speed_trend(altitude_gain_limit=18)
+    partial_plot_average_bpm_speed_trend=partial(plot_average_bpm_speed_trend, altitude_gain_limit=18)
+    plot_settings(partial_plot_average_bpm_speed_trend,
+                  title='Average Heart Rate vs Average Speed',
+                  xlabel='Average Speed (km/h)',
+                  ylabel='Average Heart Rate (bpm)',
+                  filename='average_bpm_speed.png',
+                  grid=False, save=False)
     plt.legend(['Runs','Global trend','Low elevation Trend'])
-    plt.title('Average Heart Rate vs Average Speed')
-    plt.colorbar(label='Elevation Gain (m)')
-    plt.xlabel('Average Speed (km/h)')
-    plt.ylabel('Average Heart Rate (bpm)')
-    plt.savefig('./Results/average_bpm_speed.png', dpi=300)
+    
+    plt.figure()
+    scatter_average_cadence()
+    plot_settings(plot_cadence_trend,
+                  title='Average Cadence over years',
+                  xlabel='date',
+                  ylabel='Average Cadence (step/min)',
+                  filename='average_cadence_over_time.png',
+                  grid=False, save=True)
+    plt.xticks(size=8)
+    plt.savefig('./Results/average_cadence_over_time.png',dpi=300)
+
+    plt.figure()
+    scatter_average_bpm()
+    plot_settings(plot_bpm_trend,
+                  title='Average heartrate over years',
+                  xlabel='date',
+                  ylabel='Average heartrate (pulse/min)',
+                  filename='average_heartrate_over_time.png',
+                  grid=False, save=True)
+    plt.xticks(size=8)
+    plt.savefig('./Results/average_heartrate_over_time.png',dpi=300)   
+
+    plt.figure()
+    scatter_efficiency()
+    plot_settings(plot_efficiency_trend,
+                  title='Average efficiency over years',
+                  xlabel='date',
+                  ylabel='Average efficiency (pulse/km)',
+                  filename='average_efficiency_over_time.png',
+                  grid=False, save=True)
+    plt.xticks(size=8)
+    plt.savefig('./Results/average_efficiency_over_time.png',dpi=300)
+
+    plt.figure()
+    plot_monthly_distance(save=True)
+
+    plt.figure()
+    scatter_average_pace()
+    plot_settings(plot_pace_trend,
+                  title='Average pace over years',
+                  xlabel='date',
+                  ylabel='Average pace (min/km)',
+                  filename='average_pace_over_time.png',
+                  grid=False, save=True)
+    plt.xticks(size=8)
+    plt.savefig('./Results/average_pace_over_time.png',dpi=300) 
+
+    plt.figure()
+    scatter_running_effectiveness()
+    plot_settings(plot_running_effectiveness_trend,
+                  title='Running effectiveness over years',
+                  xlabel='date',
+                  ylabel="Running effectiveness (speed/watts/kg)",
+                  filename='running_effectiveness_over_time.png',
+                  grid=False,save=True)
+    
+    show_global_statistics()
+
     plt.show()
